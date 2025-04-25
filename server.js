7a97a1cd65d659bf39a28f2bf7b3bd70ec3097e7
hdinceler <55253026+hdinceler@users.noreply.github.com>
2025-04-25 21:10:32 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,33 @@
 //server.js:
 console.clear();
 const WebSocket = require('ws');
-const{v4:uuidv4}=require('uuid'); // benzersiz user id ler için
+const {v4:uuidv4}=require('uuid'); // benzersiz user id ler için
+
 const wsServer=new WebSocket.Server({port:3000})
+
 const wsClients=[];
+
 wsServer.on('connection',(socket)=>{
+
     socket.id=uuidv4();
     wsClients.push(socket.id)
  
     console.log("biri geldi:", socket.id);
     
-    socket.send('Hoşgeldin');
+    //socket.send('Hoşgeldin');
+
     socket.on('message',(message)=>{
         console.log('gelen mesaj:',message.toString());
         console.log(wsClients.length," kişi var");
 
         wsServer.clients.forEach(client=>{
-            if(client.readyState===WebSocket.OPEN){
+            if( client!==socket &&   client.readyState===WebSocket.OPEN){
                 client.send(message.toString())
             }
         })
     })
     socket.on('close',(socket)=>{
+        console.log("biri gitti")
         const index= wsClients.indexOf(socket.id);
         if(index!==-1) wsClients.splice(index,1)
     })
